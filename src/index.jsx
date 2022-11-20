c18cce706702b177e2772f69c1cc88e20d0534a1
Vanillma <Accounts@Vanillma.ir>
2022-11-20 23:37:45 +0330

    👀 'index.jsx' Imports Cleared

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Create a 'root' Element
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// After a 'root' Element Created.
+// Render main Component (App) with 'Strict'
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
