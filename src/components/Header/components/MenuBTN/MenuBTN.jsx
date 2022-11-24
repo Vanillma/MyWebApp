80868812c428f2cbeed372dfa78a506926ef8f89
Vanillma <Accounts@Vanillma.ir>
2022-11-24 18:40:46 +0330

    🍔 Created 'Menu-Icon'

--- src/components/Header/components/MenuBTN/MenuBTN.jsx
+++ src/components/Header/components/MenuBTN/MenuBTN.jsx
@@ -3,7 +3,7 @@ import "./menuBTN.css";
 function MenuBTN() {
   return (
     <div className="btnWrapper">
-      <div className="btnLines centerLine"></div>
+      <div className="btnLines"></div>
     </div>
   );
 }
