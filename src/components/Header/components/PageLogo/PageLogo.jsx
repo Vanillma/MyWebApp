fe94bce8b85fbaa94417ce1332ad67acd68360cf
Vanillma <Accounts@Vanillma.ir>
2022-11-25 09:47:45 +0330

    🍌 'PageLogo' Component Fixed.

--- src/components/Header/components/PageLogo/PageLogo.jsx
+++ src/components/Header/components/PageLogo/PageLogo.jsx
@@ -1,19 +1,7 @@
 import "./pageLogo.css";
 
 function PageLogo() {
-  return (
-    <div className="wPageLogo">
-      <a
-        href="https://example.com"
-        className="logoLink"
-      >
-        <h2 className="logo">
-          <span className="logoP1">V</span>
-          <span className="logoP2">nima</span>
-        </h2>
-      </a>
-    </div>
-  );
+  return (<div className="wPageLogo"><a href="https://example.com"className="logoLink"><h2 className="logo"><span className="logoP1">V</span><span className="logoP2">nima</span></h2></a></div>);
 }
 
 export default PageLogo;
