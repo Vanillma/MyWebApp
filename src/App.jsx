cb9044e85ae8cd805da6e14ea2a22d88e79598fe
Vanillma <Accounts@Vanillma.ir>
2022-11-25 10:06:43 +0330

    🔧 'Header' Improved

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import "normalize.css"
+import "normalize.css";
 import Header from "./components/Header/Header.jsx";
 import "./app.css";
 
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <h1 className="headTest">React</h1>
     </div>
   );
 }
