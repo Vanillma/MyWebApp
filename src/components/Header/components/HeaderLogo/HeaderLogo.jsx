db7f0f77c7dd25895e4aaa45aa3831e518b33f1c
Vanillma <Accounts@Vanillma.ir>
2022-11-26 06:24:21 +0330

    🪜 Class Names Fixed.

--- src/components/Header/components/HeaderLogo/HeaderLogo.jsx
+++ src/components/Header/components/HeaderLogo/HeaderLogo.jsx
@@ -1,7 +1,7 @@
 import "./headerLogo.css";
 
-function PageLogo() {
-  return (<div className="wPageLogo"><a href="https://example.com"className="logoLink"><h2 className="logo"><span className="logoP1">V</span><span className="logoP2">nima</span></h2></a></div>);
+function HeaderLogo() {
+  return (<div><a href="https://example.com"className="logoLink"><h2 className="logo"><span className="logoP1">V</span><span className="logoP2">nima</span></h2></a></div>);
 }
 
-export default PageLogo;
+export default HeaderLogo;
